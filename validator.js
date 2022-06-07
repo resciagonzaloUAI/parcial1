6b9534f3cecb46b7e9ca07608bdcb28b2624cb1c
resciagonzaloUAI <gonzalonicolas.rescia@alumnos.uai.edu.ar>
2022-06-06 23:44:54 -0300

    Nuevos cambios

--- validator.js
+++ validator.js
@@ -6,21 +6,18 @@ window.onload = function() {
             var a = document.forms["myForm"]["fapellido"].value;
             longape = a.length;
                 if (longape > 2) {
-                        var c = document.forms["myForm"]["fpasswd"].value;
-                        longcon = c.length;
-                        if (longcon > 7){
                             var emailID = document.forms["myForm"]["fmail"].value;
                             atpos = emailID.indexOf("@");
                             dotpos = emailID.lastIndexOf(".");
                             if (atpos < 1 || ( dotpos - atpos < 2 )) {
                                 alert("El email no es correcto");
                                 alert(emailID);
-                                document.forms["myForm"]["fmail"].focus() ;
+                                document.forms["myForm"]["fmail"].focus();
                                 return false;
                             } else {
-                                var t = document.forms["myForm"]["fte"].value;
-                                longtel = t.length;
-                                    if (longtel > 09) {
+                                var edad = document.forms["myForm"]["fedad"].value;
+                                longedad = edad.length;
+                                    if (longedad > 0) {
                                         var loc = document.forms["myForm"]["fdire"].value;
                                         longdire = loc.length;
                                         if (longdire > 10) {
@@ -34,13 +31,8 @@ window.onload = function() {
                                     document.forms["myForm"]["fte"].focus() ;
                                     return false;}
                             }
-                        }
-                         else {
-                        alert("La contraseña debe ser mayor a 7 letras");
-                        alert(longcon);
-                        document.forms["myForm"]["fpasswd"].focus() ;
-                        return false;
-                            }           
+                        
+                               
                 } else {
                     alert("El apellido debe ser mayor a 2 letras");
                     alert(longape);
@@ -58,3 +50,57 @@ window.onload = function() {
     }
     
 };
+
+function nombre() {
+var x = document.forms["myForm"]["fname"].value;
+longitudn = x.length;
+if (longitudn > 2) {
+} else {
+    alert("El nombre debe ser mayor a 2 letras");
+    alert(longitudn);
+    document.forms["myForm"]["fname"].focus() ;
+    return false;
+}
+}
+
+function apellido() {
+    var apellido = document.forms["myForm"]["fapellido"].value;
+    longitudApe = apellido.length;
+    if (longitudn > 2) {
+    } else {
+        alert("El apellido debe ser mayor a 2 letras");
+        alert(longitudApe);
+        document.forms["myForm"]["fapellido"].focus() ;
+        return false;
+    }
+    }
+
+    function correo() {
+        var emailID = document.forms["myForm"]["fmail"].value;
+        atpos = emailID.indexOf("@");
+        dotpos = emailID.lastIndexOf(".");
+        if (atpos < 1 || ( dotpos - atpos < 2 )) {
+            alert("El email no es correcto");
+            alert(emailID);
+            document.forms["myForm"]["fmail"].focus();
+            return false; }
+            else {}
+    }
+
+    function edad () {
+        var edad = document.forms["myForm"]["fedad"].value;
+        longEdad = edad.length;
+        if (longEdad > 0)  {
+            if (edad < 17) {
+                alert ("Debe ser mayor a 18 para registrarse.")
+                alert(longEdad);
+                document.forms["myForm"]["fapellido"].focus() ;
+                return false;
+            } else {}
+        } else {
+            alert("Debe completar la edad");
+            alert(longitudApe);
+            document.forms["myForm"]["fapellido"].focus() ;
+            return false;
+        }
+    }
